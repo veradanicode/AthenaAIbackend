791a53dd8980b129525b8a02100cccbe748a222d
Vera D <veradanicode@gmail.com>
2025-09-07 00:19:56 +0100

    update commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from  express.Router();
+const router = express.Router();
 import {registerUser,loginUser} from '../controllers/auth-controller';
 import authMiddleware from '../middleware/auth'
 
