e0893b4675dd0c0921dffed2d03d90a29d9b6860
Vera D <veradanicode@gmail.com>
2025-09-06 23:14:13 +0100

    update commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,15 +2,16 @@
 import jwt from 'jsonwebtoken';
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.headers('Authorization');
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
 
   try {
-    const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
-    req.user = decoded;
+    const decodedToken = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
+    req.user = decodedToken;
     next();
+    
   } catch (err) {
     res.status(401).json({ message: 'Token is not valid' });
   }
