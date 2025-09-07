cabb8edf92d6ef2887c23b7b1b9aa90295707401
Vera D <veradanicode@gmail.com>
2025-09-07 18:38:05 +0100

    fix bug in user Route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 const router = express.Router();
 import {registerUser,loginUser} from '../controllers/auth-controller';
-import authMiddleware from '../middleware/auth'
 
 // routes
 router.post('/register', registerUser);
