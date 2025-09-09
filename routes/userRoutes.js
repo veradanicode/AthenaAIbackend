64109d95826e21d3d724d04d72ec5d91554dfa51
Vera D <veradanicode@gmail.com>
2025-09-09 22:59:39 +0100

    fix userRoutes bug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import {registerUser,loginUser} from '../controllers/auth-controller';
+import {registerUser,loginUser} from '../controllers/auth-controller.js';
 
 // routes
 router.post('/register', registerUser);
